fc1feb7f41e72d69fb52ec22a75b5eb585b56c2c
martincomparetto <martin.comparetto@mail.com>
2021-02-02 20:01:09 -0300

    Views

--- InstWeb/Controllers/AlumnosController.cs
+++ InstWeb/Controllers/AlumnosController.cs
@@ -1,23 +1,41 @@
+using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InstWeb.Controllers
 {
     public class AlumnosController : Controller
     {
-        public IActionResult Crear()
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Guardar(string nombre, string apellido, string email, string estadoCivil, int rangoEdad)
+        public IActionResult Guardar([Bind("Nombre,Apellido,Email,Domicilio,Telefono,FechaNacimiento,DNI,Legajo")] Alumno alumno)
+        {
+            alumno.ID = Guid.NewGuid();
+
+            return View("Details", alumno);
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(IFormCollection data)
         {
             Alumno nuevoAlumno = new Alumno();
-            nuevoAlumno.Nombre = nombre;
-            nuevoAlumno.Apellido = apellido;
-            nuevoAlumno.Email = email;
-            nuevoAlumno.EstadoCivil = estadoCivil;
-            nuevoAlumno.RangoEdad = rangoEdad;
+            // nuevoAlumno.Nombre = nombre;
+            // nuevoAlumno.Apellido = apellido;
+            // nuevoAlumno.Email = email;
 
             return View(nuevoAlumno);
         }
